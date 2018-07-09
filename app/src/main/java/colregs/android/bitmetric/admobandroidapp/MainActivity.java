cc5fcb6ca33b6fcdda8c9f3cf62f2a583fe2470c
Sunder <sunderrra31@gmail.com>
2018-07-09 18:22:16 +0530

    Initial commit

---
+++ app/src/main/java/colregs/android/bitmetric/admobandroidapp/MainActivity.java
@@ -0,0 +1,57 @@
+package colregs.android.bitmetric.admobandroidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+
+public class MainActivity extends AppCompatActivity {
+    private AdView mAdView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (TextUtils.isEmpty(getString(R.string.banner_home_footer))) {
+            Toast.makeText(getApplicationContext(), "Please mention your Banner Ad ID in strings.xml", Toast.LENGTH_LONG).show();
+            return;
+        }
+        AdSize customAdSize = new AdSize(150, 150);
+        mAdView = findViewById(R.id.adView);
+        mAdView.setAdSize(customAdSize);
+        mAdView.setAdUnitId(getString(R.string.banner_home_footer));
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                // Check the LogCat to get your test device ID
+                .addTestDevice("B3EEABB8EE11C2BE770B684D95219ECB")
+                .build();
+        mAdView.loadAd(adRequest);
+    }
+    @Override
+    public void onPause() {
+        if (mAdView != null) {
+            mAdView.pause();
+        }
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mAdView != null) {
+            mAdView.resume();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mAdView != null) {
+            mAdView.destroy();
+        }
+        super.onDestroy();
+    }
+}
